5ddf506be2c682a9a8c4c527cb052dbd8f30865c
antoan-97 <antoan.todorov@abv.bg>
2024-04-27 14:34:55 +0300

    Setup mongoose

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/cubicle-may-2023'
+const uri = 'mongodb://127.0.0.1:27017/cubicle'
 
-async function dbConnect() {
+async function dbConnect(){
     await mongoose.connect(uri);
 }
 
