424e429eda1410ecc277efa15c2abd6bc1e84b96
antoan-97 <antoan.todorov@abv.bg>
2024-04-28 13:33:40 +0300

    add creaete accessories page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoriesController = require('./controllers/accessoriesController');
 
 router.use(homeController);
 router.use('/cubes', cubeController);
+router.use('/accessories', accessoriesController);
 router.get('*', (req, res) => {
     res.redirect('/404');
 });
