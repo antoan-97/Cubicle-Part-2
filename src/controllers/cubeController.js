f5f4b6ac0f7a5fe03d0bd6d0188bdee33579cc51
antoan-97 <antoan.todorov@abv.bg>
2024-05-01 13:51:39 +0300

    show remaining accessories only

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -38,7 +38,7 @@ router.get('/:cubeId/details', async (req, res) => {
 router.get('/:cubeId/attach-accessory', async (req, res) => {
 
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    const accessories = await accessoriesManager.getAll().lean();
+    const accessories = await accessoriesManager.getOthers(cube.accessories).lean();
     const hasAccessories = accessories.length > 0 
     res.render('accessories/attach', { cube, accessories, hasAccessories });
 
