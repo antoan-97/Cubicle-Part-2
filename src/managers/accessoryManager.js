bf201733c6f4bf5113d8d8a283f38d8a97e6b553
antoan-97 <antoan.todorov@abv.bg>
2024-05-01 13:58:01 +0300

    update function name

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,4 +2,4 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
 exports.create = (accessoryData) => Accessory.create(accessoryData);
-exports.getOthers = (accessoryId) => Accessory.find({_id: { $nin: accessoryId}});
+exports.getExceptThese = (accessoryId) => Accessory.find({_id: { $nin: accessoryId}});
