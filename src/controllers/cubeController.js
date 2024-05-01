562462427e856f6a44e167dfb958578fc707d820
antoan-97 <antoan.todorov@abv.bg>
2024-05-01 13:09:46 +0300

    add relation from cube to accessory

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -44,4 +44,13 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
 
 })
 
+router.post('/:cubeId/attach-accessory', async ( req, res) =>{
+    const { accessory: accessoryId } = req.body;
+    const cubeId = req.params.cubeId;
+
+    await cubeManager.attachAccessory(cubeId, accessoryId);
+
+    res.redirect(`/cubes/${cubeId}/details`)
+})
+
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -29,3 +29,12 @@ exports.create = async (cubeData) => {
 
     return cube;
 };
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+    // return Cube.findByIdAndUpdate(cubeId, { $push: { accessories: accessoryId}});
+
+    const cube = await Cube.findById(cubeId);
+    cube.accessories.push(accessoryId);
+
+    return cube.save();
+}
