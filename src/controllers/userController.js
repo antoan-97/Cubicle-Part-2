781ffebd9a7dd07e34560b1dc0667389641710da
antoan-97 <antoan.todorov@abv.bg>
2024-05-04 14:33:21 +0300

    save user with validated password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', (req, res) =>{
-    const userData = req.body
-    // const {name, password, repeatPassword } = req.body
-    res.render('/users/login');
+router.post('/register', async (req, res) =>{
+    const { username, password, repeatPassword } = req.body
+
+    await userManager.register({username, password, repeatPassword});
+    res.redirect('/users/login');
 })
 module.exports = router
--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    username: String,
     password: String
 });
 
-const User = mongoose.model('User',userSchema);
+userSchema.virtual('repeatPassword')
+    .set(function(value){
+        if(value !== this.password) {
+            throw new mongoose.MongooseError('Password missmatch!');
+        }
+    })
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
