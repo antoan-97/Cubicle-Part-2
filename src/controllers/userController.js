c8c556d725195e353b323f7c770f86febc466c02
antoan-97 <antoan.todorov@abv.bg>
2024-05-04 14:10:51 +0300

    add basic userSchema

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,12 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
 router.get('/register', (req, res) => {
-    res.render('users/register')
+    res.render('users/register');
 });
 
+router.post('/register', (req, res) =>{
+    const userData = req.body
+    // const {name, password, repeatPassword } = req.body
+    res.render('/users/login');
+})
 module.exports = router
