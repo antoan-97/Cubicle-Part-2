406dd915db4deb5568f76c55686577ef813e750e
antoan-97 <antoan.todorov@abv.bg>
2024-05-06 00:15:33 +0300

    User login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,21 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username,password) => {
+
+    const user = await User.findOne({ username });
+
+    if(!user){
+        throw new Error('Inalid username or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if(!isValid){
+        throw new Error('Inalid username or password');
+    }
+
+    return user;
+}
