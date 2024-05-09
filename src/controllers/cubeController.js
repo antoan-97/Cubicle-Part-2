7285cf81b8933a4df947a51ad5349886f4c7de6b
antoan-97 <antoan.todorov@abv.bg>
2024-05-09 13:33:28 +0300

    create function for difficulty options

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -79,5 +79,24 @@ router.post('/:cubeId/edit', async (req,res) =>{
     res.redirect(`/cubes/${req.params.cubeId}/details`);
 })
 
+function generateDifficultyOptions(difficultyLevel) {
+    const titles = [
+        'Very Easy',
+        'Easy',
+        'Medium (Standart 3x3)',
+        'Intermediate',
+        'Expert',
+        'Hardcore',
+    ];
+
+    const options = titles.map((title,index) => ({
+        title: `${index + 1 } - ${title}`,
+        value: index + 1,
+        selected: Number(difficultyLevel) === index+1
+    }))
+
+    return option
+}
+
 
 module.exports = router;
